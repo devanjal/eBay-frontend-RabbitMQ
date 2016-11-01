7fa536f2e60b1989ec2cffb6e6eb310e10425a50
Devanjal Kotia <devanjal@Devanjals-MacBook-Pro.local>
2016-10-31 23:55:00 -0700

    ebay rmqp backend

--- Ebay_MarketPalace_App/routes/signup.js
+++ Ebay_MarketPalace_App/routes/signup.js
@@ -3,6 +3,8 @@
  */
 var mongo = require("./mongo");
 var mongoURL = "mongodb://localhost:27017/ebay";
+var ejs = require("ejs");
+var mq_client = require('../rpc/client');
 
 exports.checkSignup = function(req,res){
 	// These two variables come from the form on
@@ -13,25 +15,45 @@ exports.checkSignup = function(req,res){
 	var password = req.param("password");
 	console.log(password +" is the object");
 	var json_responses;
+	var msg_payload = {"type":"signup","first_name":first_name ,"last_name": last_name, "email":email,"password":password};
 
-	mongo.connect(mongoURL, function(){
-		console.log('Connected to mongo at: ' + mongoURL);
-		var coll = mongo.collection('user');
+	//console.log("POST Request for signup :"+name +" "+newUserEmail+" "+newUserPassword);
 
-		coll.insert({first_name: first_name, last_name:last_name,email: email, password:password}, function(err, user){
-			if (user) {
-				// This way subsequent requests will know the user is logged in.
-				// req.session.username = user.username;
-				//console.log(req.session.username +" is the session");
-				json_responses = {"statusCode" : 200};
-				res.send(json_responses);
+	mq_client.make_request('login_queue',msg_payload,function(err,results){
+		console.log(results);
 
-			} else {
-				console.log("returned false");
-				json_responses = {"statusCode" : 401};
-				res.send(json_responses);
+		if(err){
+			throw err;
+		}else{
+			if(results.code == 200){
+				console.log("User account created.");
+				res.send({"status":"Success"});
 			}
-		});
+			else{
+				console.log("User account not created");
+				res.send({"status":"Fail"});
+			}
+		}
+	});
+
+	// mongo.connect(mongoURL, function(){
+	// 	console.log('Connected to mongo at: ' + mongoURL);
+	// 	var coll = mongo.collection('user');
+    //
+	// 	coll.insert({first_name: first_name, last_name:last_name,email: email, password:password}, function(err, user){
+	// 		if (user) {
+	// 			// This way subsequent requests will know the user is logged in.
+	// 			// req.session.username = user.username;
+	// 			//console.log(req.session.username +" is the session");
+	// 			json_responses = {"statusCode" : 200};
+	// 			res.send(json_responses);
+    //
+	// 		} else {
+	// 			console.log("returned false");
+	// 			json_responses = {"statusCode" : 401};
+	// 			res.send(json_responses);
+	// 		}
+	// 	});
 	});
 };
 
